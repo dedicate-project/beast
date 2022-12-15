a20d546f108d2478adc3ef8d24987968204cb734
fairlight1337 <jan.winkler.84@gmail.com>
2022-12-15 10:30:19 +0100

    Adding program tests and increasing coverage

---
+++ tests/program.cpp
@@ -0,0 +1,54 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+#include <beast/program.hpp>
+
+TEST_CASE("retrieving_1_byte_too_many_from_program_throws", "program") {
+  beast::Program prg(0);
+  bool threw = false;
+  try {
+    prg.getData1(0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("retrieving_2_bytes_too_many_from_program_throws", "program") {
+  beast::Program prg(0);
+  bool threw = false;
+  try {
+    prg.getData2(0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("retrieving_4_bytes_too_many_from_program_throws", "program") {
+  beast::Program prg(0);
+  bool threw = false;
+  try {
+    prg.getData4(0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("adding_too_large_string_table_entry_instruction_throws", "program") {
+  beast::Program prg(0);
+  const std::string entry = "Entry";
+  bool threw = false;
+  try {
+    prg.setStringTableEntry(0, entry);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
