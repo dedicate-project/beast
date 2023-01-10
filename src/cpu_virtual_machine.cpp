efa1a93486f9b29cc41f1bb79463d02ba55535dc
fairlight1337 <jan.winkler.84@gmail.com>
2023-01-10 15:50:51 +0100

    Adding const qualifiers to time functions

--- src/cpu_virtual_machine.cpp
+++ src/cpu_virtual_machine.cpp
@@ -10,12 +10,12 @@
 #endif
 
 #ifdef _MSC_VER
-struct tm* gmtime_r(time_t* _clock, struct tm* _result) {
+struct tm* gmtime_r(const time_t* _clock, struct tm* _result) {
     _gmtime64_s(_result, _clock);
     return _result;
 }
 
-struct tm* localtime_r(time_t* _clock, struct tm* _result) {
+struct tm* localtime_r(const time_t* _clock, struct tm* _result) {
     _localtime64_s(_result, _clock);
     return _result;
 }
