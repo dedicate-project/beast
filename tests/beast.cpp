f5ece1b41c6b7a2065e5fded22e6f06de3122d84
fairlight1337 <jan.winkler.84@gmail.com>
2023-01-14 23:17:35 +0100

    Added test for general global BEAST functions

---
+++ tests/beast.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+#include <beast/beast.hpp>
+
+TEST_CASE("beast_version_is_not_0_0_0", "beast") {
+  const auto version = beast::getVersion();
+
+  // Require at least one portion of the version to be > 0.
+  REQUIRE(((version[0] > 0) || (version[1] > 0) || (version[2] > 0)) == true);
+}
