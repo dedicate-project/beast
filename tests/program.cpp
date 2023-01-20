a6e6e5ca7a4ef96849595925a61bc25e4e8bf0d2
fairlight1337 <jan.winkler.84@gmail.com>
2023-01-20 13:09:22 +0100

    Greatly sped up test compilation/linkage
    Split out Catch2 main function linkage into individual unit to prevent
    repeated linkage. Big speed improvement!

--- tests/program.cpp
+++ tests/program.cpp
@@ -1,6 +1,3 @@
-#define CATCH_CONFIG_MAIN
-#define CATCH_CONFIG_FAST_COMPILE
-
 #include <catch2/catch.hpp>
 
 #include <beast/beast.hpp>
---
+++ tests/tests_main.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+
+#include <catch2/catch.hpp>
+
+/* This file is used to optimize linking speed for Catch2 tests. It implements the main function
+   provided by CATCH_CONFIG_MAIN and is then linked into the individual tests.
+
+   For Reference, see:
+   https://github.com/catchorg/Catch2/blob/v2.x/docs/slow-compiles.md */
