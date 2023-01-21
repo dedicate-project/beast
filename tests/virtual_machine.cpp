0258f4c51ceb95d42a11ac75ec4d31c781c2646b
fairlight1337 <jan.winkler.84@gmail.com>
2023-01-21 11:46:42 +0100

    Adding message severity tests

---
+++ tests/virtual_machine.cpp
@@ -0,0 +1,162 @@
+#include <catch2/catch.hpp>
+
+#include <beast/beast.hpp>
+
+namespace {
+
+class MockVirtualMachine : public beast::VirtualMachine {
+ public:
+  void sendDebugMessage(const std::string& message) {
+    debug(message);
+  }
+
+  void sendInfoMessage(const std::string& message) {
+    info(message);
+  }
+
+  void sendWarningMessage(const std::string& message) {
+    warning(message);
+  }
+
+  void sendErrorMessage(const std::string& message) {
+    error(message);
+  }
+
+  void sendPanicMessage(const std::string& message) {
+    panic(message);
+  }
+
+  const std::string& getCachedMessage(beast::VirtualMachine::MessageSeverity severity) {
+    return cache_[severity];
+  }
+
+  bool step(beast::VmSession& /*session*/) override {
+    return true;
+  }
+
+ protected:
+   void message(beast::VirtualMachine::MessageSeverity severity, const std::string& message)
+       override {
+     cache_[severity] = message;
+   }
+
+ private:
+  std::map<beast::VirtualMachine::MessageSeverity, std::string> cache_;
+};
+
+}  // namespace
+
+TEST_CASE("when_message_severity_set_to_debug_all_messages_are_printed", "virtual_machine") {
+  const std::string debug_message = "Debug";
+  const std::string info_message = "Info";
+  const std::string warning_message = "Warning";
+  const std::string error_message = "Error";
+  const std::string panic_message = "Panic";
+
+  MockVirtualMachine mock_vm;
+  mock_vm.setMinimumMessageSeverity(beast::VirtualMachine::MessageSeverity::Debug);
+  mock_vm.sendDebugMessage(debug_message);
+  mock_vm.sendInfoMessage(info_message);
+  mock_vm.sendWarningMessage(warning_message);
+  mock_vm.sendErrorMessage(error_message);
+  mock_vm.sendPanicMessage(panic_message);
+
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Debug) == debug_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Info) == info_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Warning)
+          == warning_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Error) == error_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Panic) == panic_message);
+}
+
+TEST_CASE("when_message_severity_set_to_info_the_right_messages_are_printed", "virtual_machine") {
+  const std::string debug_message = "Debug";
+  const std::string info_message = "Info";
+  const std::string warning_message = "Warning";
+  const std::string error_message = "Error";
+  const std::string panic_message = "Panic";
+
+  MockVirtualMachine mock_vm;
+  mock_vm.setMinimumMessageSeverity(beast::VirtualMachine::MessageSeverity::Info);
+  mock_vm.sendDebugMessage(debug_message);
+  mock_vm.sendInfoMessage(info_message);
+  mock_vm.sendWarningMessage(warning_message);
+  mock_vm.sendErrorMessage(error_message);
+  mock_vm.sendPanicMessage(panic_message);
+
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Debug).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Info) == info_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Warning)
+          == warning_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Error) == error_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Panic) == panic_message);
+}
+
+TEST_CASE("when_message_severity_set_to_warn_the_right_messages_are_printed", "virtual_machine") {
+  const std::string debug_message = "Debug";
+  const std::string info_message = "Info";
+  const std::string warning_message = "Warning";
+  const std::string error_message = "Error";
+  const std::string panic_message = "Panic";
+
+  MockVirtualMachine mock_vm;
+  mock_vm.setMinimumMessageSeverity(beast::VirtualMachine::MessageSeverity::Warning);
+  mock_vm.sendDebugMessage(debug_message);
+  mock_vm.sendInfoMessage(info_message);
+  mock_vm.sendWarningMessage(warning_message);
+  mock_vm.sendErrorMessage(error_message);
+  mock_vm.sendPanicMessage(panic_message);
+
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Debug).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Info).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Warning)
+          == warning_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Error) == error_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Panic) == panic_message);
+}
+
+TEST_CASE("when_message_severity_set_to_error_the_right_messages_are_printed", "virtual_machine") {
+  const std::string debug_message = "Debug";
+  const std::string info_message = "Info";
+  const std::string warning_message = "Warning";
+  const std::string error_message = "Error";
+  const std::string panic_message = "Panic";
+
+  MockVirtualMachine mock_vm;
+  mock_vm.setMinimumMessageSeverity(beast::VirtualMachine::MessageSeverity::Error);
+  mock_vm.sendDebugMessage(debug_message);
+  mock_vm.sendInfoMessage(info_message);
+  mock_vm.sendWarningMessage(warning_message);
+  mock_vm.sendErrorMessage(error_message);
+  mock_vm.sendPanicMessage(panic_message);
+
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Debug).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Info).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Warning).empty()
+          == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Error) == error_message);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Panic) == panic_message);
+}
+
+TEST_CASE("when_message_severity_set_to_panic_the_right_messages_are_printed", "virtual_machine") {
+  const std::string debug_message = "Debug";
+  const std::string info_message = "Info";
+  const std::string warning_message = "Warning";
+  const std::string error_message = "Error";
+  const std::string panic_message = "Panic";
+
+  MockVirtualMachine mock_vm;
+  mock_vm.setMinimumMessageSeverity(beast::VirtualMachine::MessageSeverity::Panic);
+  mock_vm.sendDebugMessage(debug_message);
+  mock_vm.sendInfoMessage(info_message);
+  mock_vm.sendWarningMessage(warning_message);
+  mock_vm.sendErrorMessage(error_message);
+  mock_vm.sendPanicMessage(panic_message);
+
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Debug).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Info).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Warning).empty()
+          == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Error).empty() == true);
+  REQUIRE(mock_vm.getCachedMessage(beast::VirtualMachine::MessageSeverity::Panic) == panic_message);
+}
