46a9579a141e4413c8ae8eb4ab307bc99050c947
fairlight1337 <jan.winkler.84@gmail.com>
2023-01-24 21:28:47 +0100

    Documented program factory base and random program factory

--- include/beast/program_factory_base.hpp
+++ include/beast/program_factory_base.hpp
@@ -8,9 +8,11 @@ namespace beast {
 
 /**
  * @class ProgramFactoryBase
- * @brief NEEDS DOCUMENTATION
+ * @brief Base class for program factories
  *
- * TODO(fairlight1337): Document this class.
+ * Program factories generate programs based on high level parameters and criteria. A concrete
+ * implementation of a factory can have a multitude of parameterization options that define the
+ * characteristics of the generated programs.
  */
 class ProgramFactoryBase {
  public:
@@ -26,10 +28,19 @@ class ProgramFactoryBase {
    * @fn ProgramFactoryBase::generate
    * @brief Generates a program based on the factory's semantics
    *
-   * TODO(fairlight1337): Document this function.
+   * Generates a Program instance that follows the factory's concrete implementation's
+   * characteristics. The given parameters that define a runtime-parameterization are used to make
+   * an educated guess on how to make the generated program executable. For example, only variables
+   * may be declared or used whose indices lie within the expected memory size to avoid illogical
+   * program traits. The concrete use of these parameters, if any, depends on the respective
+   * implementation of the factory though.
    *
-   * @param size The size of the program to generate
-   * @return The generated program
+   * @param size The maximum size of the program to generate, in bytes
+   * @param memory_size The memory size the generated program would be executed with
+   * @param string_table_size The string table size the generated program would be executed with
+   * @param string_table_item_length The string table item length the generated program would be
+   *        executed with
+   * @return The generated Program instance
    */
   virtual Program generate(
       uint32_t size, uint32_t memory_size, uint32_t string_table_size,
