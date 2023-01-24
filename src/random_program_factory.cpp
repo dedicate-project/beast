fb842c665bb2b6052522971ff82caab99896eb23
fairlight1337 <jan.winkler.84@gmail.com>
2023-01-24 15:58:21 +0100

    Implemented remaining random program factory operators
    All known operators are implemented in the RandomProgramFactory class
    now. Programs that are generated have a chance to be using valid ranges
    of variables, string table indices, and rel/abs jump addresses. Also,
    only valid operator codes are generated, and the operands applied to
    them make sense in the scope of the respective operand.

--- src/random_program_factory.cpp
+++ src/random_program_factory.cpp
@@ -6,8 +6,8 @@
 namespace beast {
 
 Program RandomProgramFactory::generate(
-    uint32_t size, uint32_t memory_size, uint32_t /*string_table_size*/,
-    uint32_t /*string_table_item_length*/) {
+    uint32_t size, uint32_t memory_size, uint32_t string_table_size,
+    uint32_t string_table_item_length) {
   Program prg(size);
 
   std::random_device random_device;
@@ -24,6 +24,20 @@ Program RandomProgramFactory::generate(
       -static_cast<int32_t>(static_cast<double>(size) * 0.5),
       static_cast<int32_t>(static_cast<double>(size) * 0.5));
   std::uniform_int_distribution<int32_t> abs_addr_distribution(0, size);
+  std::uniform_int_distribution<int32_t> string_table_index_distribution(0, string_table_size);
+
+  // A random string that fits into the string table, with characters ranging from ASCII 33-126.
+  const auto string_item_generator = [&mersenne_engine, &string_table_item_length]() {
+      std::string random_string;
+      std::uniform_int_distribution<> length_distribution(0, string_table_item_length);
+      std::uniform_int_distribution<> char_distribution(33, 126);
+      const int32_t length = length_distribution(mersenne_engine);
+      for (uint32_t idx = 0; idx < length; idx++) {
+        const char c = char_distribution(mersenne_engine);
+        random_string += c;
+      }
+      return random_string;
+  };
 
   // Random true/false switch.
   const auto bool_generator =
@@ -35,21 +49,30 @@ Program RandomProgramFactory::generate(
       [&var_distribution, &mersenne_engine]() {
         return var_distribution(mersenne_engine);
       };
+  // A random value from the signed int32 range.
   const auto int32_generator =
       [&int32_distribution, &mersenne_engine]() {
         return int32_distribution(mersenne_engine);
       };
+  // A random value from the signed int8 range.
   const auto int8_generator =
       [&int8_distribution, &mersenne_engine]() {
         return int8_distribution(mersenne_engine);
       };
+  // A random relative address in the range [-size/2, size/2).
   const auto rel_addr_generator =
       [&rel_addr_distribution, &mersenne_engine]() {
         return rel_addr_distribution(mersenne_engine);
       };
+  // A random valid absolute address in the range [0, size];
   const auto abs_addr_generator =
       [&abs_addr_distribution, &mersenne_engine]() {
         return abs_addr_distribution(mersenne_engine);
+      };
+  // A random valid string table index.
+  const auto string_table_index_generator =
+      [&string_table_index_distribution, &mersenne_engine]() {
+        return string_table_index_distribution(mersenne_engine);
       };
 
   while (true) {
@@ -355,33 +378,48 @@ Program RandomProgramFactory::generate(
     } break;
 
     case OpCode::SetStringTableEntry: {
+      fragment.setStringTableEntry(string_table_index_generator(), string_item_generator());
     } break;
 
     case OpCode::PrintStringFromStringTable: {
+      fragment.printStringFromStringTable(string_table_index_generator());
     } break;
 
     case OpCode::LoadStringTableLimitIntoVariable: {
+      fragment.loadStringTableLimitIntoVariable(var_generator(), bool_generator());
     } break;
 
     case OpCode::LoadStringTableItemLengthLimitIntoVariable: {
+      fragment.loadStringTableItemLengthLimitIntoVariable(var_generator(), bool_generator());
     } break;
 
     case OpCode::SetVariableStringTableEntry: {
+      fragment.setVariableStringTableEntry(
+          var_generator(), bool_generator(), string_item_generator());
     } break;
 
     case OpCode::PrintVariableStringFromStringTable: {
+      fragment.printVariableStringFromStringTable(var_generator(), bool_generator());
     } break;
 
     case OpCode::LoadVariableStringItemLengthIntoVariable: {
+      fragment.loadVariableStringItemLengthIntoVariable(
+          var_generator(), bool_generator(), var_generator(), bool_generator());
     } break;
 
     case OpCode::LoadVariableStringItemIntoVariables: {
+      fragment.loadVariableStringItemIntoVariables(
+          var_generator(), bool_generator(), var_generator(), bool_generator());
     } break;
 
     case OpCode::LoadStringItemLengthIntoVariable: {
+      fragment.loadStringItemLengthIntoVariable(
+          string_table_index_generator(), var_generator(), bool_generator());
     } break;
 
     case OpCode::LoadStringItemIntoVariables: {
+      fragment.loadStringItemIntoVariables(
+          string_table_index_generator(), var_generator(), bool_generator());
     } break;
 
     case OpCode::PushVariableOnStack: {
