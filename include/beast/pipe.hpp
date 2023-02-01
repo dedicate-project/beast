837097a8052d8a91f3792c96efce6c07c481cab1
fairlight1337 <jan.winkler.84@gmail.com>
2023-02-01 23:22:24 +0100

    Added GALGO-2.0 3rdparty lib and some initial infra for pipelines

---
+++ include/beast/pipe.hpp
@@ -0,0 +1,25 @@
+#ifndef BEAST_PIPE_HPP_
+#define BEAST_PIPE_HPP_
+
+namespace beast {
+
+/**
+ * @class Pipe
+ * @brief NEEDS DOCUMENTATION
+ *
+ * TODO(fairlight1337): Document this class.
+ */
+class Pipe {
+ public:
+  /**
+   * @class Pipe::~Pipe
+   * @brief NEEDS DOCUMENTATION
+   *
+   * TODO(fairlight1337): Document this function.
+   */
+  virtual ~Pipe() = default;
+};
+
+}  // namespace beast
+
+#endif  // BEAST_PIPE_HPP_
---
+++ src/pipe.cpp
@@ -0,0 +1,7 @@
+#include <beast/pipe.hpp>
+
+// GALGO-2.0
+#include <Galgo.hpp>
+
+namespace beast {
+}  // namespace beast
