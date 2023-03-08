5919f63de46167dd65fc7ac6ebe2f2f05b93ccad
fairlight1337 <jan.winkler.84@gmail.com>
2023-03-08 16:32:34 +0100

    Adding Pipeline tests

--- tests/pipeline.cpp
+++ tests/pipeline.cpp
@@ -46,3 +46,88 @@ TEST_CASE("adding_a_pipe_to_a_pipeline_twice_throws", "pipeline") {
 
   REQUIRE(threw == true);
 }
+
+TEST_CASE("adding_a_connection_with_source_pipe_not_in_pipeline_throws", "pipeline") {
+  beast::Pipeline pipeline;
+  std::shared_ptr<beast::Pipe> pipe0 = std::make_shared<MockPipe>();
+  std::shared_ptr<beast::Pipe> pipe1 = std::make_shared<MockPipe>();
+  pipeline.addPipe(pipe1);
+
+  bool threw = false;
+  try {
+    pipeline.connectPipes(pipe0, 0, pipe1, 0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("adding_a_connection_with_destination_pipe_not_in_pipeline_throws", "pipeline") {
+  beast::Pipeline pipeline;
+  std::shared_ptr<beast::Pipe> pipe0 = std::make_shared<MockPipe>();
+  pipeline.addPipe(pipe0);
+  std::shared_ptr<beast::Pipe> pipe1 = std::make_shared<MockPipe>();
+
+  bool threw = false;
+  try {
+    pipeline.connectPipes(pipe0, 0, pipe1, 0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("adding_a_connection_with_either_pipe_not_in_pipeline_throws", "pipeline") {
+  beast::Pipeline pipeline;
+  std::shared_ptr<beast::Pipe> pipe0 = std::make_shared<MockPipe>();
+  std::shared_ptr<beast::Pipe> pipe1 = std::make_shared<MockPipe>();
+
+  bool threw = false;
+  try {
+    pipeline.connectPipes(pipe0, 0, pipe1, 0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("adding_the_same_connection_twice_throws", "pipeline") {
+  beast::Pipeline pipeline;
+  std::shared_ptr<beast::Pipe> pipe0 = std::make_shared<MockPipe>();
+  pipeline.addPipe(pipe0);
+  std::shared_ptr<beast::Pipe> pipe1 = std::make_shared<MockPipe>();
+  pipeline.addPipe(pipe1);
+
+  pipeline.connectPipes(pipe0, 0, pipe1, 0);
+
+  bool threw = false;
+  try {
+    pipeline.connectPipes(pipe0, 0, pipe1, 0);
+  } catch(...) {
+    threw = true;
+  }
+
+  REQUIRE(threw == true);
+}
+
+TEST_CASE("adding_a_connection_and_retrieving_it_works", "pipeline") {
+  beast::Pipeline pipeline;
+  std::shared_ptr<beast::Pipe> pipe0 = std::make_shared<MockPipe>();
+  pipeline.addPipe(pipe0);
+  std::shared_ptr<beast::Pipe> pipe1 = std::make_shared<MockPipe>();
+  pipeline.addPipe(pipe1);
+
+  pipeline.connectPipes(pipe0, 0, pipe1, 1);
+
+  const auto& connections = pipeline.getConnections();
+  REQUIRE(connections.size() == 1);
+
+  const auto& connection = *connections.begin();
+  REQUIRE(connection.source_pipe == pipe0);
+  REQUIRE(connection.source_slot_index == 0);
+  REQUIRE(connection.destination_pipe == pipe1);
+  REQUIRE(connection.destination_slot_index == 1);
+}
