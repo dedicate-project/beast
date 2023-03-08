8e8803d5ca1a8ad7360b98c618ddb0b9120f5eef
fairlight1337 <jan.winkler.84@gmail.com>
2023-03-08 15:15:17 +0100

    More exception handling

--- src/serve.cpp
+++ src/serve.cpp
@@ -19,36 +19,43 @@ int main(int argc, char** argv) {
   uint32_t http_port = 9192;
 
   // Parse command line arguments
-  CLI::App cli{"BEAST Serve"};
   try {
+    CLI::App cli{"BEAST Serve"};
+
     cli.add_option("--html_root", html_root, "HTML root directory");
     cli.add_option("--http_port", html_root, "The HTTP port to serve on");
 
     CLI11_PARSE(cli, argc, argv);
   } catch(const std::runtime_error& exception) {
-    std::cerr << "Failed to prepare CLI parser: " << exception.what() << std::endl;
+    std::cerr << "Failed to parse arguments: " << exception.what() << std::endl;
     return EXIT_FAILURE;
   } catch(...) {
-    std::cerr << "Failed to prepare CLI parser due to unknown issue" << std::endl;
+    std::cerr << "Failed to parse arguments due to an unknown issue" << std::endl;
     return EXIT_FAILURE;
   }
 
   // Set up web serving environment
   crow::SimpleApp app;
-  CROW_ROUTE(app, "/<path>")
-      ([&](const crow::request& /*req*/, crow::response& res, const std::string& path) {
-         const std::string full_path = html_root + "/" + path;
-         try {
-           if (std::ifstream(full_path)) {
-             res.set_static_file_info(full_path);
-           } else {
-             res.code = 404;
+
+  // Set up serving routes
+  try {
+    CROW_ROUTE(app, "/<path>")
+        ([&](const crow::request& /*req*/, crow::response& res, const std::string& path) {
+           const std::string full_path = html_root + "/" + path;
+           try {
+             if (std::ifstream(full_path)) {
+               res.set_static_file_info(full_path);
+             } else {
+               res.code = 404;
+             }
+           } catch(...) {
+             res.code = 500;
            }
-         } catch(...) {
-           res.code = 500;
-         }
-         res.end();
-       });
+           res.end();
+         });
+  } catch(...) {
+    std::cerr << "Failed to set up erver routes" << std::endl;
+  }
 
   // Start application
   try {
