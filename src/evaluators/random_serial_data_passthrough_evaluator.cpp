67d1577a55b8690354b3ba119957b6b75643faa2
fairlight1337 <jan.winkler.84@gmail.com>
2023-03-09 09:29:42 +0100

    Fixed an issue with random repetitions and a code smell

--- src/evaluators/random_serial_data_passthrough_evaluator.cpp
+++ src/evaluators/random_serial_data_passthrough_evaluator.cpp
@@ -20,43 +20,24 @@ double RandomSerialDataPassthroughEvaluator::evaluate(const VmSession& session) 
   std::uniform_int_distribution<> distribution(
       std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max());
 
-  std::vector<int32_t> values;
-  for (uint32_t idx = 0; idx < data_count_; ++idx) {
-    values.push_back(distribution(mersenne_twister));
-  }
-
-  beast::CpuVirtualMachine virtual_machine;
-  virtual_machine.setSilent(true);
-
   double worst_result = 1.0;
   for (uint32_t repeat = 0; repeat < repeats_; ++repeat) {
+    std::vector<int32_t> values;
+    for (uint32_t idx = 0; idx < data_count_; ++idx) {
+      values.push_back(distribution(mersenne_twister));
+    }
+
     // Make a copy because we're changing the state of the session.
     VmSession work_session = session;
+    uint32_t correct_forwards = 0;
 
     work_session.setVariableBehavior(0, beast::VmSession::VariableIoBehavior::Input);
     work_session.setVariableBehavior(1, beast::VmSession::VariableIoBehavior::Output);
 
-    uint32_t value_index = 0;
-    uint32_t step_count = 0;
-    uint32_t correct_forwards = 0;
-
-    work_session.setVariableValue(0, true, values[value_index]);
+    work_session.setVariableValue(0, true, values[0]);
 
     try {
-      do {
-        if (work_session.hasOutputDataAvailable(1, true)) {
-          if (work_session.getVariableValue(1, true) == values[value_index]) {
-            correct_forwards++;
-          }
-          value_index++;
-          if (value_index < values.size()) {
-            work_session.setVariableValue(0, true, values[value_index]);
-          }
-        }
-        step_count++;
-      } while(value_index < values.size() &&
-              step_count < max_steps_ &&
-              virtual_machine.step(work_session, false));
+      correct_forwards = runProgram(work_session, values);
     } catch(...) {
       return 0.0;
     }
@@ -69,6 +50,33 @@ double RandomSerialDataPassthroughEvaluator::evaluate(const VmSession& session) 
   }
 
   return worst_result;
+}
+
+uint32_t RandomSerialDataPassthroughEvaluator::runProgram(
+    VmSession& work_session, const std::vector<int32_t>& values) const {
+  beast::CpuVirtualMachine virtual_machine;
+  virtual_machine.setSilent(true);
+
+  uint32_t value_index = 0;
+  uint32_t step_count = 0;
+  uint32_t correct_forwards = 0;
+
+  do {
+    if (work_session.hasOutputDataAvailable(1, true)) {
+      if (work_session.getVariableValue(1, true) == values[value_index]) {
+        correct_forwards++;
+      }
+      value_index++;
+      if (value_index < values.size()) {
+        work_session.setVariableValue(0, true, values[value_index]);
+      }
+    }
+    step_count++;
+  } while(value_index < values.size() &&
+          step_count < max_steps_ &&
+          virtual_machine.step(work_session, false));
+
+  return correct_forwards;
 }
 
 }  // namespace beast
