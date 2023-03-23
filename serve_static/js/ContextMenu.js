af905862dc227c3acdbaf0fd13694c3da0bbe330
fairlight1337 <jan.winkler.84@gmail.com>
2023-03-23 23:34:02 +0100

    Add context menu to pipeline canvas
    Still buggy, but basics are there

---
+++ serve_static/js/ContextMenu.js
@@ -0,0 +1,36 @@
+const { createElement: e } = React;
+const { Menu, MenuItem } = MaterialUI;
+
+export function ContextMenu({ show, position, onClose, menuItems }) {
+  const menuRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (show && menuRef.current) {
+      const { x, y } = position;
+      menuRef.current.style.transform = `translate(${x}px, ${y}px)`;
+    }
+  }, [show, position]);
+
+  const handleClose = (e) => {
+    e.stopPropagation();
+    onClose();
+  };
+
+  return e(
+    Menu,
+    {
+      anchorEl: menuRef.current,
+      open: show,
+      onClose: onClose,
+      style: { zIndex: 1500 },
+      transformOrigin: {
+        vertical: 'top',
+        horizontal: 'left',
+      },
+    },
+    menuItems.map((item, index) =>
+      e(MenuItem, { key: index, onClick: onClose }, item),
+    ),
+  );
+}
+
