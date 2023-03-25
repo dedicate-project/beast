0d2b28c5d2613803bfa9bfb2f98f57ba7adeb5cf
fairlight1337 <jan.winkler.84@gmail.com>
2023-03-25 21:16:47 +0100

    Adding pipeline manager docs and tests, and fixing other tests

--- docs/helpers.rst
+++ docs/helpers.rst
@@ -6,7 +6,14 @@ This section describes the helper classes and functions available in the BEAST p
 FilesystemHelper Class
 ----------------------
 
-:ref:`The FilesystemHelper class` provides functionality for working with files in a specific
+:ref:`The FilesystemHelper Class` provides functionality for working with files in a specific
 directory. It is designed to be used for saving and loading BEAST pipelines that are stored as JSON
 objects. The class provides functions for saving and deleting models, as well as loading all models
 in a directory. For more information, please see the `FilesystemHelper` class documentation below.
+
+PipelineManager Class
+---------------------
+
+:ref:`The PipelineManager Class` provides an efficient way to manage and organize pipelines in the
+Beast library. It handles the creation, storage, and retrieval of pipelines, streamlining the
+processing of complex data workflows.
--- include/beast/pipeline_manager.hpp
+++ include/beast/pipeline_manager.hpp
@@ -11,21 +11,48 @@
 
 namespace beast {
 
+/**
+ * @class PipelineManager
+ * @brief Manages a collection of pipelines.
+ */
 class PipelineManager {
  public:
+  /**
+   * @struct PipelineDescriptor
+   * @brief Describes a pipeline with its ID, name, filename, and Pipeline object.
+   */
   struct PipelineDescriptor {
-    uint32_t id;
-    std::string name;
-    std::string filename;
-    Pipeline pipeline;
+    uint32_t id;          //!< Unique identifier for the pipeline.
+    std::string name;     //!< Display name of the pipeline.
+    std::string filename; //!< Filename of the pipeline's serialized data.
+    Pipeline pipeline;    //!< The Pipeline object.
   };
 
+  /**
+   * @brief Constructor that initializes the PipelineManager with a storage path.
+   * @param storage_path Path to the storage directory.
+   */
   PipelineManager(const std::string& storage_path);
 
+  /**
+   * @brief Creates a new pipeline and adds it to the collection.
+   * @param name The display name for the new pipeline.
+   * @return The unique identifier for the new pipeline.
+   */
   uint32_t createPipeline(const std::string& name);
 
+  /**
+   * @brief Gets a reference to a pipeline by its ID.
+   * @param id The unique identifier of the desired pipeline.
+   * @return A reference to the pipeline descriptor with the given ID.
+   * @throws std::invalid_argument if the pipeline with the given ID is not found.
+   */
   PipelineDescriptor& getPipelineById(uint32_t id);
 
+  /**
+   * @brief Gets a const reference to the list of pipeline descriptors.
+   * @return A const reference to the list of pipeline descriptors.
+   */
   const std::list<PipelineDescriptor>& getPipelines() const;
 
  private:
@@ -33,9 +60,9 @@ class PipelineManager {
 
   uint32_t getFreeId() const;
 
-  FilesystemHelper filesystem_;
+  FilesystemHelper filesystem_; //!< Filesystem helper for managing model storage.
 
-  std::list<PipelineDescriptor> pipelines_;
+  std::list<PipelineDescriptor> pipelines_; //!< Collection of pipelines.
 };
 
 }  // namespace beast
