de2cfd6ab679d2878b1fb2182846adc99fd08a14
fairlight1337 <jan.winkler.84@gmail.com>
2023-04-01 21:35:12 +0200

    Moved version information into its own file and added string version

--- include/beast/beast.hpp
+++ include/beast/beast.hpp
@@ -15,7 +15,7 @@
 #include <beast/program.hpp>
 #include <beast/random_program_factory.hpp>
 #include <beast/time_functions.hpp>
-#include <beast/version.h>
+#include <beast/version.hpp>
 #include <beast/vm_session.hpp>
 
 #include <beast/evaluators/aggregation_evaluator.hpp>
@@ -25,19 +25,5 @@
 #include <beast/evaluators/runtime_statistics_evaluator.hpp>
 
 #include <beast/pipes/evaluator_pipe.hpp>
-
-namespace beast {
-
-/**
- * @brief Returns the version of the BEAST library
- *
- * The version returned by this function is an array containing the major, minor, and patch version
- * parts in the format {MAJOR, MINOR, PATCH} and follows the semantic versioning scheme.
- *
- * @return A 3-element array holding the major, minor, and patch version parts.
- */
-[[nodiscard]] std::array<uint8_t, 3> getVersion() noexcept;
-
-}  // namespace beast
 
 #endif  // BEAST_BEAST_HPP_
