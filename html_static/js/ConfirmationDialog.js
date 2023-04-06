77ea790265cdbf6072451092c647371a8aa5c5d6
fairlight1337 <jan.winkler.84@gmail.com>
2023-04-06 09:31:40 +0200

    Applied JavaScript formatting

--- html_static/js/ConfirmationDialog.js
+++ html_static/js/ConfirmationDialog.js
@@ -1,20 +1,12 @@
-const { createElement: e } = React;
-const { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } = MaterialUI;
+const {createElement : e} = React;
+const {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button} = MaterialUI;
 
-export function ConfirmationDialog({ open, onClose, onConfirm, title, content }) {
-  return e(
-    Dialog,
-    {
-      open: open,
-      onClose: onClose,
-    },
-    e(DialogTitle, null, title),
-    e(DialogContent, null, e(DialogContentText, null, content)),
-    e(
-      DialogActions,
-      null,
-      e(Button, { onClick: onClose, color: "primary" }, "No"),
-      e(Button, { onClick: onConfirm, color: "secondary" }, "Yes")
-    )
-  );
+export function ConfirmationDialog({open, onClose, onConfirm, title, content}) {
+  return e(Dialog, {
+    open : open,
+    onClose : onClose,
+  },
+           e(DialogTitle, null, title), e(DialogContent, null, e(DialogContentText, null, content)),
+           e(DialogActions, null, e(Button, {onClick : onClose, color : "primary"}, "No"),
+             e(Button, {onClick : onConfirm, color : "secondary"}, "Yes")));
 }
