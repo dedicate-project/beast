13df093c7963d6c07fe6f3c8f469e8236dfa5584
fairlight1337 <jan.winkler.84@gmail.com>
2023-04-22 11:39:59 +0200

    Updated maze dep and began integration of the maze evaluator

--- src/evaluators/maze_evaluator.cpp
+++ src/evaluators/maze_evaluator.cpp
@@ -9,13 +9,49 @@
 
 namespace beast {
 
+namespace {
+maze::Maze getSolvableMaze(uint32_t rows, uint32_t cols, double difficulty, uint32_t max_tries) {
+  uint32_t tries = 0;
+
+  do {
+    maze::Maze maze(rows, cols, difficulty);
+    if (maze.isSolvable()) {
+      return maze;
+    }
+    tries++;
+  } while (tries < max_tries);
+
+  throw std::runtime_error(
+      std::string("Failed to generate a solvable maze with these parameters: ") +
+      "rows=" + std::to_string(rows) + ", cols=" + std::to_string(cols) +
+      ", difficulty=" + std::to_string(difficulty) + ", max_tries=" + std::to_string(max_tries));
+}
+} // namespace
+
 MazeEvaluator::MazeEvaluator(uint32_t rows, uint32_t cols, double difficulty, uint32_t max_steps)
     : rows_{rows}, cols_{cols}, difficulty_{difficulty}, max_steps_{max_steps} {}
 
-double MazeEvaluator::evaluate(const VmSession& /*session*/) {
-  // TODO(fairlight1337): Implement this evaluation function.
+double MazeEvaluator::evaluate(const VmSession& session) {
+  maze::Maze target_maze = getSolvableMaze(rows_, cols_, difficulty_, 10);
 
-  return 0.0;
+  VmSession local_session = session;
+  CpuVirtualMachine virtual_machine;
+  virtual_machine.setSilent(true);
+  const uint32_t max_steps = 1000;
+  uint32_t current_steps = 0;
+  while (virtual_machine.step(local_session, false)) {
+    // TODO(fairlight1337): Implement this evaluation function: Feed current sight and player state
+    // into maze, and get move commands out.
+    static_cast<void>(target_maze);
+
+    current_steps++;
+    if (current_steps > max_steps) {
+      return 0.0;
+    }
+  }
+
+  double score = 0.0;
+  return score;
 }
 
 uint32_t MazeEvaluator::getRows() const { return rows_; }
